3d551d800ab7ffde21d5b23a3680dec1ae0bccf5
it-smt <smt_razrab@mail.ru>
2025-01-06 13:28:25 +0500

    initial commit

---
+++ app/main.py
@@ -0,0 +1,58 @@
+from datetime import date
+from typing import List, Optional
+
+from fastapi import FastAPI, Query, Depends
+
+from pydantic import BaseModel
+import uvicorn
+
+app = FastAPI()
+
+
+class HotelSearchArgs:
+    def __init__(
+        self,
+        location: str,
+        date_from: date,
+        date_to: date,
+        has_spa: Optional[bool] = None,
+        stars: Optional[int] = Query(None, ge=1, le=5),
+    ):
+        self.location = location
+        self.date_from = date_from
+        self.date_to = date_to
+        self.has_spa = has_spa
+        self.stars = stars
+
+
+class SHotel(BaseModel):
+    address: str
+    name: str
+    stars: int
+
+
+@app.get("/hotels")
+def get_hotels(search_args: HotelSearchArgs = Depends()) -> List[SHotel]:
+    hotels = [
+        {
+            "address": "ул. Гагарина, 1, Алтай",
+            "name": "Super Hotel",
+            "stars": 5,
+        }
+    ]
+    return hotels
+
+
+class SBooking(BaseModel):
+    room_id: int
+    date_from: date
+    date_to: date
+
+
+@app.post("/bookings")
+def add_booking():
+    pass
+
+
+if __name__ == "__main__":
+    uvicorn.run(app="main:app", reload=True)
