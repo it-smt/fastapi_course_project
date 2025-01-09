8958dbec949648346d4c3f01b83f74385fdd1619
it-smt <smt_razrab@mail.ru>
2025-01-09 22:33:49 +0500

    config

--- app/config.py
+++ app/config.py
@@ -1,12 +1,53 @@
+from typing import Literal
+
+from pydantic import model_validator
 from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
+    MODE: Literal["DEV", "TEST", "PROD"]
+
     DB_HOST: str
     DB_PORT: int
     DB_USER: str
     DB_PASS: str
     DB_NAME: str
+    DATABASE_URL: str
+
+    @model_validator(mode="before")
+    def get_database_url(cls, v):
+        v["DATABASE_URL"] = (
+            f"postgresql+asyncpg://{v['DB_USER']}:{v['DB_PASS']}@{v['DB_HOST']}:{v['DB_PORT']}/{v['DB_NAME']}"
+        )
+        return v
+
+    TEST_DB_HOST: str
+    TEST_DB_PORT: int
+    TEST_DB_USER: str
+    TEST_DB_PASS: str
+    TEST_DB_NAME: str
+    TEST_DATABASE_URL: str
+
+    @model_validator(mode="before")
+    def get_test_database_url(cls, v):
+        v["TEST_DATABASE_URL"] = (
+            f"postgresql+asyncpg://{v['TEST_DB_USER']}:{v['TEST_DB_PASS']}@{v['TEST_DB_HOST']}:{v['TEST_DB_PORT']}/{v['TEST_DB_NAME']}"
+        )
+        return v
+
+    SMTP_HOST: str
+    SMTP_PORT: int
+    SMTP_USER: str
+    SMTP_PASS: str
+
+    REDIS_HOST: str
+    REDIS_PORT: int
+
+    SECRET_KEY: str
+    ALGORITHM: str
 
     class Config:
         env_file = ".env"
+
+
+settings = Settings()
